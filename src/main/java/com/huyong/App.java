c9de3236c2c30165eed7a003d129ffbecc64a53c
huyong <huyong1023@gmail.com>
2016-01-15 11:11:45 +0800

    init

---
+++ src/main/java/com/huyong/App.java
@@ -0,0 +1,13 @@
+package com.huyong;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
