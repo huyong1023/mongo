a0ab3c7faa11ed03c30cc1b68f8be2720f8c3c85
huyong <huyong1023@gmail.com>
2016-01-20 17:58:05 +0800

    el :wq

--- src/main/java/com/huyong/mongo/SimpleTest.java
+++ src/main/java/com/huyong/mongo/SimpleTest.java
@@ -1,7 +1,78 @@
 package com.huyong.mongo;
 
+import com.huyong.mongo.domain.Tree;
+import com.huyong.mongo.repository.NatureRepositoryImpl;
+import com.huyong.mongo.repository.Repository;
+import com.mongodb.*;
+
+
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Set;
+
+
+import static java.util.Arrays.asList;
 /**
  * Created by huyong on 1/15/16.
  */
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class SimpleTest {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+                "classpath:/applicationContext.xml");
+
+        Repository<Tree> repository = context.getBean(NatureRepositoryImpl.class);
+
+        // cleanup collection before insertion
+        repository.dropCollection();
+
+        // create collection
+        repository.createCollection();
+
+        repository.saveObject(new Tree("1", "Apple Tree", 10));
+
+        repository.saveObject(new Tree("2", "Orange Tree", 3));
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 50; i++) {
+            repository.setObject("1", "name", "BananaTree");
+            repository.setObject("1", "name", "KiwiFruitTree");
+        }
+
+        long end = System.currentTimeMillis();
+        long duration = end - start;
+        System.out.format("1. 使用set修改100次，共用时%s毫秒n", duration);
+
+        start = System.currentTimeMillis();
+
+        for (int i = 0; i < 50; i++) {
+            repository.incObject("1", "age", 1);
+            repository.incObject("1", "age", 2);
+        }
+
+        end = System.currentTimeMillis();
+        duration = end - start;
+        System.out.format("2. 使用inc修改100次，共用时%s毫秒n", duration);
+
+        start = System.currentTimeMillis();
+
+        for (int i = 0; i < 50; i++) {
+            repository.pushObject("1", "fruit", "Banana" + i);
+            repository.pushObject("1", "fruit", "Kiwi" + i);
+        }
+
+        end = System.currentTimeMillis();
+        duration = end - start;
+        System.out.format("3. 使用push修改100次，共用时%s毫秒n", duration);
+    }
 }
--- src/main/java/com/huyong/mongo/Test.java
+++ src/main/java/com/huyong/mongo/Test.java
@@ -1,7 +1,29 @@
 package com.huyong.mongo;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import java.util.Set;
+
 /**
  * Created by huyong on 1/20/16.
  */
 public class Test {
+
+    public static void main(String[] args) {
+        try {
+            MongoClient mongoClient = new MongoClient("192.168.11.22");
+            // 连接到数据库
+            DB db = mongoClient.getDB("test");
+            System.out.println("Connect to database successfully");
+            Boolean flag = db.collectionExists("tree");
+
+            System.out.println("Collection mycol selected successfully:" + flag);
+            System.out.println("Collection created successfully");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
 }
